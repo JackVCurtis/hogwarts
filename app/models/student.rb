c96739e711acb24b7fe062e97b7c0e9a4ed1ad84
JackCurtis <jack.v.curtis@gmail.com>
2015-01-08 18:45:13 -0500

    w06d04

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,13 @@
-def Student < ActiveRecord::Base
+class Student < ActiveRecord::Base
   belongs_to :house
+
+  before_save :assign_house
+
+  private
+
+  def assign_house
+    houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+    house = House.find_by(name: houses.sample)
+    self.house = house
+  end
 end
